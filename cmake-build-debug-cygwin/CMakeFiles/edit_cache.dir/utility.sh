030b934e87f30589b293b372cd1592b8561f005d
Rokas Jodka <100208970+Uni1Q@users.noreply.github.com>
2024-09-11 18:53:26 +0100

    Delete cmake-build-debug-cygwin directory

--- cmake-build-debug-cygwin/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/c/Users/rokas/CLionProjects/untitled/cmake-build-debug-cygwin
-/cygdrive/c/Users/rokas/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
--- cmake-build-debug-cygwin/CMakeFiles/rebuild_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/c/Users/rokas/CLionProjects/untitled/cmake-build-debug-cygwin
-/cygdrive/c/Users/rokas/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
